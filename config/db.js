e9e2f9fbcedd9500f0aa832da64be33b4b11d226
Freddy Drennan <drennanfreddy@gmail.com>
2020-03-05 20:32:56 -0700

    update

--- config/db.js
+++ config/db.js
@@ -1,20 +1,19 @@
-const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get('mongoURI');
-
+const mongoose = require("mongoose");
+const config = require("config");
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
+      useUnifiedTopology: true,
       useFindAndModify: false
     });
-
-    console.log('MongoDB Connected...');
+    console.log("MongoDB Connected");
   } catch (err) {
-    console.error(err.message);
-    process.exit(1);
+    console.log(err.message);
   }
 };
+
+const db = config.get("mongoURI");
 
 module.exports = connectDB;
